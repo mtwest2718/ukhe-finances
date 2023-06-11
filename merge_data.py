b02e1e9e71dc5d26587a3629c5ab88d1a291af5d
Matthew T West <m.t.west@exeter.ac.uk>
2023-06-11 20:29:43 +0100

    add category filtering to parse_table function

--- merge_data.py
+++ merge_data.py
@@ -9,12 +9,15 @@ def make_negative(x):
     else:
         return x
 
-def rename_category_col(long):
+def rename_category_col(tbl_id, long):
     if tbl_id==12:
         drop_cols = long.columns[4]
     elif tbl_id==6:
+        long = long[long['source of fees']=='Total']
         drop_cols = list(long.columns[[2,4]])
     else:
+        if tbl_id==9:
+            long = long[long['type of asset']=='Total capital expenditure']
         drop_cols = list(long.columns[3:-2])
     # dropping category metadata columns
     if drop_cols:
@@ -22,20 +25,60 @@ def rename_category_col(long):
 
     return long.rename(columns={long.columns[-2]: 'category'})
 
-def filter_categories(tbl_id, df):
+def filter_categories(tbl_id, unfiltered):
     cats = {
-        1: ['Total income', 'Total expenditure'],
-        3: [],
-        4: [],
-        6: [],
-        7: [],
-        9: [],
-        10: [],
-        12: [],
-        14: []
+        1: ['Total income',
+            'Total expenditure',
+            'Staff costs'],
+        3: ['Total net assets/(liabilities)',
+            'Total current assets',
+            'Bank overdrafts ',
+            'Investments ',                 # Current assets 
+            'Total creditors (amounts falling due within one year)',
+            'Cash and cash equivalents ',
+            'Deferred course fees',
+            'Other (including grant claw back)',
+            'Tax and social security costs',
+            'Total creditors (amounts falling due after more than one year)',
+            'Other (including grant claw back) '],
+        4: ['Net cash inflow from operating activities',
+            'Depreciation',
+            'Interest paid',
+            'Repayments of amounts borrowed',
+            'Interest element of finance lease and service concession payments',
+            'Capital element of finance lease and service concession payments'],
+        6: ['Total tuition fees and education contracts',
+            'Total HE course fees',
+            'Total UK fees'],
+        7: ['Total research grants and contracts',
+            'Total other income',
+            'Total donations and endowments',
+            'Total residences and catering operations (including conferences)',
+            'Funding body grants'],         # income from
+        9: ['Total actual spend',
+            'Funding body grants',          # money from. used for capital expenditures
+            'Internal funds',
+            'Other external sources'],
+        12: ['Average staff numbers (FTE) as disclosed in accounts',
+            'Total staff numbers (FTE) as disclosed in accounts',
+            'Total changes to pension provisions/ pension adjustments',
+            'Changes to pension provisions',
+            'Total salaries and wages','Salaries and wages academic staff',
+            'Salaries and wages non-academic staff',
+            'Average academic staff numbers (FTE)',
+            'Average non-academic staff numbers (FTE)']
     }
 
-def read_table(tbl_id):
+    categories = cats[tbl_id]
+    df = unfiltered[ unfiltered['category'].isin(categories) ]
+    # double use of "Funding body grants"
+    if tbl_id==7:
+        df['category'].mask(
+            df['category']=='Funding body grants','Total funding body grant income')
+    return df
+
+
+def parse_table(tbl_id):
     # name of the CSV file 
     csv_file = f"table-{tbl_id}.csv"
     print(csv_file)
@@ -69,7 +112,9 @@ def read_table(tbl_id):
     df = df.convert_dtypes()
 
     # drop excess category metadata
-    long = rename_category_col(df)
+    narrowed = rename_category_col(tbl_id, df)
+    # select only the desired categories from each file
+    long = filter_categories(tbl_id, narrowed)
 
     print(long.columns)
 
@@ -77,9 +122,10 @@ def read_table(tbl_id):
 
 
 
-tlbs = [read_table(T) for T in [1,3,4,6,7,9,10,12]]
-long_tbl = pdf.concat(tbls, ignore_index=True)
-
+tbls = [parse_table(T) for T in [1,3,4,6,7,9,12]]
+# merge tables vertically
+long_tbl = pd.concat(tbls, ignore_index=True)
+print(long_tbl)
 
 
 
